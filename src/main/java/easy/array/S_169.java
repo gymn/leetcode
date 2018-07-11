b4a82650c2e9c1398116fdcec3e9f4953cbcc2a3
hunan <450826993@qq.com>
2018-07-11 23:30:02 +0800

    20180711

--- src/main/java/easy/array/S_169.java
+++ src/main/java/easy/array/S_169.java
@@ -1,5 +1,8 @@
 package easy.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Author hunan
  * @Date 2018/6/30
@@ -7,4 +10,15 @@ package easy.array;
  * 求众数
  */
 public class S_169 {
+    public int majorityElement(int[] nums) {
+        Map<Integer,Integer> numberMap = new HashMap<>();
+        for(int i=0;i<nums.length;i++) {
+            Integer count = numberMap.getOrDefault(nums[i],0)+1;
+            if(count>=nums.length/2d){
+                return nums[i];
+            }
+            numberMap.put(nums[i],count);
+        }
+        return 0;
+    }
 }
